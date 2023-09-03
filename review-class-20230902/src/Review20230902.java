35f4189fbbd5c717759a63bdf4d965b670d4d769
tmh3014 <stan.tsang2006@gmail.com>
2023-09-03 18:30:49 +0800

    updated at 2/9

---
+++ review-class-20230902/src/Review20230902.java
@@ -0,0 +1,106 @@
+public class Review20230902 {
+    public static void main (String [] args) {
+        String box = "Bee";
+        String aaa = "Honey ";
+        System.out.println(aaa + box);
+        System.out.println(aaa + "box");
+
+        int x = 1000;
+        int salary = x * 20;
+        System.out.println(salary + x);
+        int bonus = x * 10 / 4;
+        System.out.println(salary + bonus);
+        System.out.println(3000 + salary + bonus);
+
+        System.out.println(aaa + salary);
+        System.out.println("box " + bonus / 2);
+        
+        int j = 0;
+        j = j + 1;
+        j += 1;
+        System.out.println(j);
+        j++;
+        ++j;
+        System.out.println(j);
+
+        int j2 = 5;
+        j2 = j2 - 1;
+        j2 -= 1;
+        System.out.println(j2);
+        j2--;
+        --j2;
+        System.out.println(j2);
+
+        int j3 = 3;
+        j3 = j3 * 3;
+        j3 *= 2;
+        System.out.println(j3);
+
+        int j4 = 4;
+        j4 = j4 / 2;
+        j4 /= 2;
+        System.out.println(j4);
+
+        double d = 4.5;
+        d = d + 0.3 - 0.1 * 2;
+        System.out.println(d);
+        d = d - 0.9 / 0.3;
+        System.out.println(d);
+        double d2 = 4;
+        System.out.println(d2);
+
+        boolean a = true;
+        System.out.println(a);
+        boolean a2 = false;
+        System.out.println(a2);
+
+        char a3 = 'a';
+        char a4 = 'b';
+        int a5 = a3 + a4;
+        System.out.println(a5);
+
+        byte b = -1;
+        byte b2 = -5 ;
+        byte b3 = 4;
+        int b4 = b2 + b3;
+        System.out.println(b4);
+
+        short sh = 300;
+        int r2 = sh + 500;
+        System.out.println(r2);
+
+        long l1 = 20000;
+        l1 += 5000;
+        l1 -= 2000;
+        l1 *= 5;
+        l1 /= 10;
+        System.out.println(l1);
+
+        String k2 = "Honey ";
+        k2 += "Bee";
+        System.out.println(k2);
+        String k3 = "Happy ";
+        System.out.println(k3 + "Day");
+        
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
